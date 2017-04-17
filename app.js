58b6f07f1342cd3984fb4439cb2574d4c7b42c9e
Yoga Prakoso <yogaprakoso90@gmail.com>
2017-04-17 10:37:13 +0000

    add websocket on server-side and client-side

---
+++ app.js
@@ -0,0 +1,28 @@
+var express  = require('express');
+var bodyParser = require('body-parser');
+var app      = express();
+var server   = require('http').Server(app);
+var io       = require('socket.io')(server);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req,res,next){
+    req.io = io;
+    next();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.sendfile('index.html');
+});
+
+io.on('connection', function(client){
+  console.log("Client connected");
+  client.on('event', function(data){});
+  client.on('disconnect', function(){
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(3000);
+console.log('Listen to http://localhost:3000');
